607604d09c6e42f86077e7076f371d90f9fcfb90
Unchae <dnsco987@gmail.com>
2021-06-25 16:59:25 +0900

    hi

---
+++ dist/Python_crawling4.bat
@@ -0,0 +1,3 @@
+c:
+cd \C:\Users\medici\python_base\dist
+Python_crawling4..exe 도지,비트코인
--- python_api.py
+++ python_api.py
@@ -1,5 +1,17 @@
 import requests
 import time
+import selenium.webdriver as webdriver
+import sys
+
+driver = webdriver.Chrome('C://chromedriver/chromedriver.exe')
+url_start = 'https://news.naver.com'
+
+if len(sys.argv) == 2:
+    keywords = list(sys.argv[1].split('.'))
+else:
+    keywords = ['NH은행','NH증권']
+
+
 from xml.etree import ElementTree
 from datetime import date
 from dateutil.relativedelta import relativedelta
